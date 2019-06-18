fd07cbc11b544d2e904fb0396814dbecbacf059b
zj <1071902755@qq.com>
2019-06-18 17:11:14 +0800

    增加工具类

---
+++ src/main/java/com/j1902/beststore/utils/MD5Util.java
@@ -0,0 +1,27 @@
+package com.j1902.beststore.utils;
+
+import java.security.MessageDigest;
+
+public class MD5Util {
+	// 加盐
+	private static final String slat = "bb*32@$#%^bb%$logj1902a@#￥aa";
+
+	public static String encrypt(String dataStr) {
+		try {
+			dataStr = dataStr + slat;
+			MessageDigest m = MessageDigest.getInstance("MD5");
+			m.update(dataStr.getBytes("UTF8"));
+			byte s[] = m.digest();
+			String result = "";
+			for (int i = 0; i < s.length; i++) {
+				result += Integer.toHexString((0x000000FF & s[i]) | 0xFFFFFF00).substring(6);
+			}
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+		return "";
+	}
+
+}
