ab96761835a140c0a0a38e5eccf331aa6e54f465
zj <1071902755@qq.com>
2019-06-18 17:14:02 +0800

    增加工具类

--- src/main/java/com/j1902/beststore/utils/UseUtil.java
+++ src/main/java/com/j1902/beststore/utils/UseUtil.java
@@ -1,9 +1,5 @@
 package com.j1902.beststore.utils;
 
-import com.zj.pojo.MySource;
-import com.zj.server.MySourceServer;
-import com.zj.to.SourcePaging;
-
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
@@ -48,37 +44,5 @@ public class UseUtil {
         return ipAddress;
     }
 
-    public static int getIndex(HttpServletRequest req){
-        int index = 1;
-        String index1 = req.getParameter("index");
-        if (index1 != null && !"".equals(index1)) {
-            index = Integer.parseInt(index1);
-        }
-        return index;
-    }
 
-    public static int getCount(HttpServletRequest req){
-        int count = 5;
-        String count1 = req.getParameter("count");
-        if (count1 != null && !"".equals(count1)) {
-            count = Integer.parseInt(count1);
-        }
-        return count;
-    }
-
-    public static void sort(MySourceServer mySourceServer,SourcePaging sourcePaging, String order, int index, int count, HttpSession session, HttpServletRequest req, HttpServletResponse resp, boolean b1, boolean b2) {
-        sourcePaging = mySourceServer.getAllSort(sourcePaging, index, count, session, b1, b2);
-        System.out.println("sourcePaging = " + sourcePaging);
-        setName(mySourceServer,sourcePaging);
-        session.setAttribute("ALL_SOURCES", sourcePaging);
-    }
-
-    public static void setName(MySourceServer mySourceServer,SourcePaging sourcePaging){
-        List<MySource> mySources = sourcePaging.getAllSources();
-        if (mySources != null && mySources.size() > 0) {
-            for (MySource mySource : mySources) {
-                mySource.setName(mySourceServer.getName(mySource.getNameid()));
-            }
-        }
-    }
 }
