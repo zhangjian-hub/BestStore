129180b429e7071860c38f0bfa0042fe6a5eb73c
acheng <1037615703@qq.com>
2019-06-20 12:11:11 +0800

    checkout

---
+++ src/main/java/com/j1902/beststore/service/BsItemService.java
@@ -0,0 +1,9 @@
+package com.j1902.beststore.service;
+
+import com.j1902.beststore.pojo.BsShoppingCart;
+
+import java.util.List;
+
+public interface BsItemService {
+    public List<BsShoppingCart> getBsShoppingCart(Integer id);
+}
---
+++ src/main/java/com/j1902/beststore/service/impl/BsItemServiceImpl.java
@@ -0,0 +1,13 @@
+package com.j1902.beststore.service.impl;
+
+import com.j1902.beststore.pojo.BsShoppingCart;
+import com.j1902.beststore.service.BsItemService;
+
+import java.util.List;
+
+public class BsItemServiceImpl implements BsItemService {
+    @Override
+    public List<BsShoppingCart> getBsShoppingCart(Integer id) {
+        return null;
+    }
+}
