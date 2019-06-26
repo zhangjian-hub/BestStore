b6c16d6fc16b19231ee34262d9c7207fa548cb19
zj <1071902755@qq.com>
2019-06-26 10:21:32 +0800

    后台

--- src/main/java/com/j1902/beststore/controller/AdminPageJumpController.java
+++ src/main/java/com/j1902/beststore/controller/AdminPageJumpController.java
@@ -1,8 +1,12 @@
 package com.j1902.beststore.controller;
 
+import com.j1902.beststore.service.AdminBsItemService;
 import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Map;
 
 /**
  * @author zj
@@ -13,8 +17,14 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @MapperScan("com.j1902.beststore.mapper")
 public class AdminPageJumpController {
 
+    @Autowired
+    private AdminBsItemService itemService;
+
     @RequestMapping("/toAdminIndex.back")
-    public String toAdminIndex(){
+    public String toAdminIndex(Map<String,Object> map){
+        int itemsCount = itemService.getAllItems().size();
+        map.put("ITEMS_COUNT", itemsCount);
+
         return "admin/admin-index";
     }
 
